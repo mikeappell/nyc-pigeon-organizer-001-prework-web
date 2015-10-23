9725f9a32a3c6d8ca3ec0d9a615c18c527137fe2
mikeappell <mikeappell@gmail.com>
2015-10-23 00:24:58 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attr, data1|
+    data1.each do |data2, names|
+      names.each do |name|
+        new_hash[name] = {} if new_hash[name].nil?
+        new_hash[name][attr] = new_hash[name][attr].nil? ? [data2.to_s] : new_hash[name][attr] + [data2.to_s]
+        p new_hash
+      end
+    end
+  end
+  new_hash
 end
